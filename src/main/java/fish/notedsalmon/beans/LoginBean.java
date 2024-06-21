72704bc3b7528c87c24c7e752cc6b4c3fbc51a5e
NotedSalmon <moreiraandre2001@gmail.com>
2024-06-21 17:12:21 +0100

    Used a CSS/HTML template for a login/register form and changed it to JSF.
    Should just be now adding some other features and creating the login/register functions, so it connects to the db.

--- src/main/java/fish/notedsalmon/beans/LoginBean.java
+++ src/main/java/fish/notedsalmon/beans/LoginBean.java
@@ -1,4 +1,38 @@
 package fish.notedsalmon.beans;
 
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
 public class LoginBean {
+
+    private String username;
+    private String password;
+
+    public String login(){
+        return "success";
+    }
+
+
+
+    /**
+     * Getters and setters
+     */
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/fish/notedsalmon/beans/RegisterBean.java
+++ src/main/java/fish/notedsalmon/beans/RegisterBean.java
@@ -1,4 +1,56 @@
 package fish.notedsalmon.beans;
 
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Named;
+
+@Named
+@RequestScoped
 public class RegisterBean {
+
+    private String username;
+    private String email;
+    private String password;
+    private String fullName;
+
+    public String register() {
+        return "success";
+    }
+
+
+    /**
+     * Getters and setters
+     */
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return fullName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.fullName = firstName;
+    }
+
 }
